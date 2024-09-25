f50e4e5cacbeaab031fdb85ec166285457faa9fb
EngSamiran <samiranwwcst@gmail.com>
2024-09-25 12:15:10 +0530

    Initial code added

---
+++ src/main/java/com/tcg/Micro/API/Gateway/MicroApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tcg.Micro.API.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcg/Micro/API/Gateway/MicroApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcg.Micro.API.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
